b27189a1558c2fa92ade6a2a0eea815992a6052f
racekiller <vivasjimmy@gmail.com>
2016-10-28 16:56:02 -0700

    Added new lines for data cleaning

--- KaggleTitanic.py
+++ KaggleTitanic.py
@@ -8,10 +8,10 @@ Created on Wed Oct 26 18:34:03 2016
 import csv as csv 
 import numpy as np
 
-Windows_Path = 'C:/Users/jvivas/Dropbox/Private/Personal/Github/Kaggle---Titanic-Python-Pandas/train.csv'
-Mac_Path = '/Users/jvivas/Documents/GitHub/Kaggle - Titanic Python Pandas/train.csv'
+Windows_Path = 'C:/Users/jvivas/Dropbox/Private/Personal/Github/Kaggle---Titanic-Python-Pandas'
+Mac_Path = '/Users/jvivas/Documents/GitHub/Kaggle - Titanic Python Pandas'
 
-csv_file_object = csv.reader(open(Windows_Path))
+csv_file_object = csv.reader(open(Windows_Path+'/' + 'train.csv'))
 header = csv_file_object.__next__()
 data=[]
 
@@ -40,7 +40,9 @@ type(data[0::5,5])
 
 import pandas as pd
 # For .read_csv, always use header=0 when you know row 0 is the header row
-df = pd.read_csv(Windows_Path,header=0)
+df_original_train = pd.read_csv(Windows_Path + '/' + 'train.csv', header=0)
+df_original_test = pd.read_csv(Windows_Path + '/' + 'test.csv', header=0)
+df = pd.read_csv(Windows_Path + '/' + 'train.csv', header=0)
 df.head(3)
 df.tail(3)
 
@@ -88,6 +90,7 @@ for i in range(1,4):
 # let's draw some picture
 import pylab as P
 df['Age'].hist()
+
 P.show()
 
 df['Age'].dropna().hist(bins=16, range=(0,80),alpha = 0.5)
@@ -96,10 +99,73 @@ P.show()
 # Cleaning the data
 # Creating a column into df dataframe
 df['Gender'] = 4
+
 # Here we take the first letter of the element and convert to Uppercase
 df['Gender'] = df['Sex'].map(lambda x: x[0].upper())
+
 # Now here we replace each element string with integer number in this case
 df['Gender'] = df['Sex'].map({'female': 0, 'male': 1}).astype(int)
+
 # We will do the same than above but this time we will skip the nan (null)
 df['Embarked_F'] = df['Embarked'].dropna().map({'S': 0, 'Q': 1, 'C': 2}).\
                     astype(int)
+
+# working with missing values, what to do?
+# There are some features that missing values can be converted to null but
+# there are others that can not for example the Age of the passenger in this
+# case what we can do is take the mean by class
+
+median_ages = np.zeros((2, 3))
+
+for i in range(0, 2):
+    for j in range(0, 3):
+        median_ages[i,j] = df[(df['Gender'] == i) & \
+                              (df['Pclass'] == j+1)]['Age'].dropna().median()
+
+# lets create a new column for Age so we can put in there the new values for
+# missing data
+df['AgeFill'] = df['Age']
+# Lets show the total of passenger with missing Age for specific columns
+df[ df['Age'].isnull() ][['Gender','Pclass','Age','AgeFill']].head(10)
+
+# Lets populate the dataframe with new ages
+# here we go over the dataframe and will assign the meadian age that matches
+# the Gender and Pclass in order by i and j
+for i in range(0, 2):
+    for j in range(0, 3):
+        df.loc[(df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1),
+               'AgeFill'] = median_ages[i, j]
+
+# Let's also create a feature that records whether the Age was originally
+# missing.
+df['AgeIsNull'] = pd.isnull(df.Age).astype(int)
+
+df['FamilySize'] = df['SibSp'] + df['Parch']
+
+# Deletig unnecesary columns
+# Here we can show the columns that matches specific criteria as well
+df.dtypes[df.dtypes.map(lambda x: x=='object')]
+# We can delete these columns 
+df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1) 
+# We can delete the Age column as well since we have created the better AgeFill
+df = df.drop(['Age'],axis = 1)
+
+# The final step is to convert it into a Numpy array. Pandas can always
+# send back an array using the .values method. Assign to a new variable
+# train_data:
+
+train_data = df.values
+
+# Lets do some predictions using Random Forest
+# Import the random forest package
+from sklearn.ensemble import RandomForestClassifier
+
+#Create the rando m forest onject wich will include all the parameters
+# for the fit
+forest = RandomForestClassifier(n_estimators=100)
+
+# Fit the training data to the Survived labels and create the decision trees
+forest = forest.fit(train_data[0::, 1::], train_data[0::, 0])
+
+# Take the same decision trees and run it on the test data
+output = forest.predict(test_data)
